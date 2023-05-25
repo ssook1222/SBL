896707e2d4d1f490b5a550f8e9a9141b55219a8b
ssook <60427387+ssook1222@users.noreply.github.com>
2023-05-25 05:42:56 +0000

    FIXED : Router Setting & Code Space Error Fixed

--- sql-blog/src/App.js
+++ sql-blog/src/App.js
@@ -1,7 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Main from './components/main.jsx'
+
 function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route exact path="/" element={<Main />} > </Route>
+      </Routes>
+    </BrowserRouter> 
   );
 }
 
