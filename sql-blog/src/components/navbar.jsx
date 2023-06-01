3232b3d076d380d0f44ab0ee648dd54d5b2d3c36
ssook <60427387+ssook1222@users.noreply.github.com>
2023-06-01 04:50:22 +0000

    UPDATE : 메인 페이지 수정

--- sql-blog/src/components/navbar.jsx
+++ sql-blog/src/components/navbar.jsx
@@ -10,12 +10,13 @@ function Main_view(){
         <>
             <Navbar collapseOnSelect expand="lg" variant="white">
                 <Container>
-                    <Navbar.Brand href="#home"><h3>SQL-BLOG</h3></Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Brand href="#home" id="sql_title_div"><h3 id="sql_title" 
+                    style={{color: "rgb(58, 44, 26)", fontFamily:"Georgia"}}>SQL-BLOG</h3></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" id="sql_button" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto" style={{ margin:"auto" }}>
-                        <Nav.Link href="#features" style={{ marginRight: '30px' }} >데이터베이스 입문</Nav.Link>
-                        <Nav.Link href="#pricing" style={{ marginLeft: '30px' }}>SQL 문제풀이</Nav.Link>
+                    <Nav className="me-auto" style={{ margin:"auto" }} id="sql_index">
+                        <Nav.Link id="sql1" href="#features" style={{ marginRight: '30px' }} >데이터베이스 입문</Nav.Link>
+                        <Nav.Link id="sql2" href="#pricing" style={{ marginLeft: '30px' }}>SQL 문제풀이</Nav.Link>
                     </Nav>
                     </Navbar.Collapse>
                 </Container>
--- sql-blog/src/view/main.jsx
+++ sql-blog/src/view/main.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 import Navbar from "../components/navbar"
 
+import './main.css'
+
 function Main_view(){
     return(
         <>
-            <div style = {{backgroundColor: 'black', width:'100%', height: '3px'}} ></div>
+            <div style = {{backgroundColor: 'rgb(58, 44, 26)', width:'100%', height: '3px'}} ></div>
             <Navbar></Navbar>
-            <img style = {{width: '100%'}} src='/img/sql_blog_main.png'></img>
+            <img style = {{width: '100%'}} src='/img/001.png'></img>
             
-            <div style = {{width: "60%", margin:"auto", marginBottom: "50px"}}>
+            <div id = 'contents' style = {{width: "60%", margin:"auto", marginBottom: "50px"}}>
                 <h1 style={{fontWeight:"200", fontSize:"1rem", marginTop:"5%", marginBottom:"1%"}}>
                     <i> 추천 포스팅 </i>
                 </h1>
@@ -18,66 +20,71 @@ function Main_view(){
                     SQL 관련 포스팅 중 추천하는 포스팅입니다.
                 </h5>
 
-                <div style={{display: "flex", alignItems: "center"}}>
-                    <figure className="figure" style={{width:"300px"}}>
-                        <img style={{height: '30%', boxShadow: "5px 5px 5px 5px #E2E2E2"}} 
-                            src='/img/ceft.png' 
-                            className="figure-img img-fluid rounded" 
-                            alt="..." />
-                    </figure>
-                    <div style={{marginLeft: "30px"}}>
-                        <h5 style={{marginBottom: "0"}}>SQLD, ADSP, DASP 자격증 취득 후기</h5>
-                        <br></br>
-                        <p>
-                        오늘 딱 DASP 자격증 취득을 끝내며, <br />
-                        한국데이터산업진흥원에서 주관하는 자격증들 중 빅분기(빅데이터분석기사)를 제외한 자격증 취득을 완료했다! <br />
-                            ...
-                        </p>
+                <a href="#" style={{textDecoration:"none", color:"black"}}>
+                    <div class = 'container' style={{display: "flex", alignItems: "center"}}>
+                        <figure id ="top1" className="figure" style={{width:"300px"}}>
+                            <img style={{height: '30%', boxShadow: "5px 5px 5px 5px #E2E2E2"}} 
+                                src='/img/ceft.png' 
+                                className="figure-img img-fluid rounded" 
+                                alt="..." />
+                        </figure>
+                        <div class = 'sub-container' style={{marginLeft: "30px"}}>
+                            <h5 style={{marginBottom: "0"}}>SQLD, ADSP, DASP 자격증 취득 후기</h5>
+                            <br></br>
+                            <p>
+                            오늘 딱 DASP 자격증 취득을 끝내며, <br />
+                            한국데이터산업진흥원에서 주관하는 자격증들 중 빅분기(빅데이터분석기사)를 제외한 자격증 취득을 완료했다! <br />
+                                ...
+                            </p>
+                        </div>
                     </div>
-                </div>
+                </a>
 
                 <hr />
 
-                <div style={{display: "flex", alignItems: "center", marginBottom: '0'}}>
-                    <figure className="figure" style={{width:"550px"}}>
-                        <img style={{height: '30%', boxShadow: "5px 5px 5px 5px #E2E2E2"}} 
-                            src='/img/thumb_nail.png' 
-                            className="figure-img img-fluid rounded" 
-                            alt="..." />
-                    </figure>
-                    <div style={{marginLeft: "30px"}}>
-                        <h5>상품을 구매한 회원 비율 구하기 외 3건</h5>
-                        <br></br>
-                        <p>
-                            나는 인라인뷰 대신 WITH 절이랑 EXTRACT 써서 풀었다 <br />
-                            JOIN_2021은 2021년에 가입한 사람들(분모로 쓰려고 따로 뽑음)이구
-                            PURCHASE_2021은 2021년에 가입한 사람들 중 구매한 건을 뽑아서 고걸 이제 연, 월로 각각 정렬한 쿼리문이다. 
-                            요건 또 분자로 사용했다 ㅎ <br />
-                            ...
-                        </p>
+                <a href="#" style={{textDecoration:"none", color:"black"}}>
+                    <div class = 'container' style={{display: "flex", alignItems: "center", marginBottom: '0'}}>
+                        <figure id ="top2" className="figure" style={{width:"550px"}}>
+                            <img style={{height: '30%', boxShadow: "5px 5px 5px 5px #E2E2E2"}} 
+                                src='/img/thumb_nail.png' 
+                                className="figure-img img-fluid rounded" 
+                                alt="..." />
+                        </figure>
+                        <div class = 'sub-container' style={{marginLeft: "30px"}}>
+                            <h5>상품을 구매한 회원 비율 구하기 외 3건</h5>
+                            <br></br>
+                            <p>
+                                나는 인라인뷰 대신 WITH 절이랑 EXTRACT 써서 풀었다 <br />
+                                JOIN_2021은 2021년에 가입한 사람들(분모로 쓰려고 따로 뽑음)이구
+                                PURCHASE_2021은 2021년에 가입한 사람들 중 구매한 건을 뽑아서 고걸 이제 연, 월로 각각 정렬한 쿼리문이다. 
+                                요건 또 분자로 사용했다 ㅎ <br />
+                                ...
+                            </p>
+                        </div>
                     </div>
-                </div>
+                </a>
+                <hr />
 
-                <hr></hr>
-
-                <div style={{display: "flex", alignItems: "center"}}>
-                    <figure className="figure" style={{width:"500px"}}>
-                        <img style={{height: '30%', boxShadow: "5px 5px 5px 5px #E2E2E2"}} 
-                            src='/img/database_basic.png' 
-                            className="figure-img img-fluid rounded" 
-                            alt="..." />
-                    </figure>
-                    <div style={{marginLeft: "30px"}}>
-                        <h5 style={{marginBottom: "0"}}>데이터 베이스 입문 11장</h5>
-                        <br></br>
-                        <p>
-                        데이터베이스 입장에서는 데이터를 변경하는 DML 실행 후 <br></br>
-                        COMMIT으로 데이터베이스에 반영할 지 ROLLBACK으로 실행을 취소할 지 알 수 없음.
-                        그렇기에 ROLLBACK으로 명령어 수행이 취소될 경우를 대비해 변경 전 데이터를 언두 세그먼트에 따로 저장 <br />
-                            ...
-                        </p>
+                <a href="#" style={{textDecoration:"none", color:"black"}}>
+                    <div class = 'container' style={{display: "flex", alignItems: "center"}}>
+                        <figure id ="top3" className="figure" style={{width:"500px"}}>
+                            <img style={{height: '30%', boxShadow: "5px 5px 5px 5px #E2E2E2"}} 
+                                src='/img/database_basic.png' 
+                                className="figure-img img-fluid rounded" 
+                                alt="..." />
+                        </figure>
+                        <div class = 'sub-container' style={{marginLeft: "30px"}}>
+                            <h5 style={{marginBottom: "0"}}>데이터 베이스 입문 11장</h5>
+                            <br></br>
+                            <p>
+                            데이터베이스 입장에서는 데이터를 변경하는 DML 실행 후 <br></br>
+                            COMMIT으로 데이터베이스에 반영할 지 ROLLBACK으로 실행을 취소할 지 알 수 없음.
+                            그렇기에 ROLLBACK으로 명령어 수행이 취소될 경우를 대비해 변경 전 데이터를 언두 세그먼트에 따로 저장 <br />
+                                ...
+                            </p>
+                        </div>
                     </div>
-                </div>
+                </a>
 
                 <hr></hr>
 
